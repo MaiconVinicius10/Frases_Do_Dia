8e33b4c40b74bc62cde55a9c48136aa5d5bb9657
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2023-10-30 19:49:50 -0300

    Botão exibir todas funcionando de forma correta , treino de exibir frase simples forma mais rapida de codificar

--- app/src/main/java/com/example/frasesdodia/MainActivity.java
+++ app/src/main/java/com/example/frasesdodia/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+// classe mainActivity
 public class MainActivity extends AppCompatActivity {
-
+    // Criar Atributo com array de strings
+    String[] frases ={
+        "Frase 01",
+        "Frase 02",
+        "Frase 03",
+    };
+    // metodo onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    //criando metodo
+    public void gerarFrase(View view){
+
+    }
+
+    public void exibirTodas(View view){
+        TextView texto = findViewById(R.id.textResultado);
+
+        String textoResultado="";
+        // loop for pra exibir todas as frases
+        for(String frase : frases){
+            textoResultado = textoResultado + frase + "\n";
+        }
+        texto.setText(textoResultado);
     }
 }
